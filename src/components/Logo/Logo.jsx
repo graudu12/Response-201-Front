14a44baf03bbf5553d443bc5c6a2c91cadb9b04a
SPok <s.pokotilo@gmail.com>
2025-06-27 11:46:09 +0300

    add logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import css from "./Logo.module.css";
+import logo from "../../assets/logo.svg";
 
 const Logo = () => {
   return (
     <NavLink to="/" className={css.logo}>
-      Tasteorama
+      <img src={logo} alt="Logo icon" className={css.logoIcon} />
+      <span className={css.logoText}>Tasteorama</span>
     </NavLink>
   );
 };
