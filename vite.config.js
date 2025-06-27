c17856d8f009fb794c3c00d091118ba4431bc560
Dima <graudu_12@yahoo.co.uk>
2025-06-27 16:00:27 +0100

    json_upd

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+//https: vitejs.dev/config/
 export default defineConfig({
+  base: '/Response-201-Front/',
   plugins: [react()],
   build: {
     sourcemap: true,
