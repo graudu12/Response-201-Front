63932fbd5e7ab7a1a4b10c80a51090caf4a46a93
Mr.Elijah <beztalannyj.ilia@gmail.com>
2025-07-02 21:28:15 +0200

    save button end changes

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -4,6 +4,7 @@ import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import SaveFavoriteButton from "../SaveFavoriteButton/SaveFavoriteButton";
 
 const RecipeCard = ({ recipe }) => {
   const navigate = useNavigate();
@@ -48,6 +49,7 @@ const RecipeCard = ({ recipe }) => {
         >
           Learn More
         </button>
+        <SaveFavoriteButton small recipeId={_id} />
         {isLoggedIn && <FavoriteButton recipeId={_id} />}
       </div>
     </div>
@@ -55,4 +57,3 @@ const RecipeCard = ({ recipe }) => {
 };
 
 export default RecipeCard;
-
