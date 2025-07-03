e2007ed2d4861aba8202bf8d4beb524cfcef647c
Natalya Malenko <nataljuna@gmail.com>
2025-07-03 19:42:02 +0300

    fix footer position in layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,12 +3,18 @@ import { Suspense } from "react";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 export const Layout = ({ children }) => {
   return (
     <div className={css.container}>
       <Header />
-      <Suspense fallback={null}>{children}</Suspense>
+      <main className={css.main}>
+        <Suspense fallback={null}>
+          {children}
+          <Outlet />
+        </Suspense>
+      </main>
       <Footer />
     </div>
   );
