850ab684cc69bd5b9d75e0f7a97007f34f6df7af
Natalya Malenko <nataljuna@gmail.com>
2025-07-03 20:22:58 +0300

    fix 2 footer position in layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,17 +3,13 @@ import { Suspense } from "react";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import { Outlet } from "react-router-dom";
 
 export const Layout = ({ children }) => {
   return (
     <div className={css.container}>
       <Header />
       <main className={css.main}>
-        <Suspense fallback={null}>
-          {children}
-          <Outlet />
-        </Suspense>
+        <Suspense fallback={null}>{children}</Suspense>
       </main>
       <Footer />
     </div>
