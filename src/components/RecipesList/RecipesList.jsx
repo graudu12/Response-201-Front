eeae61327b6c91d7ae08fb338e095c059741e071
Pokora Viacheslav <slavikpokora567@gmail.com>
2025-07-05 23:41:54 +0200

    add homepage

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,122 +1,31 @@
-import { useState, useEffect, useRef, useCallback } from "react";
-import { useSelector, useDispatch } from "react-redux";
+// RecipesList.jsx
+import  { forwardRef } from "react";
 import RecipeCard from "../RecipeCard/RecipeCard";
-import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
 import styles from "./RecipesList.module.css";
-import {
-  fetchRecipes,
-  toggleFavoriteRecipeAsync,
-} from "../../redux/recipes/operations";
-import Filters from "../Filters/Filters";
 import Loading from "../Loading/Loading";
 
-const RecipesList = () => {
-  const dispatch = useDispatch();
-  const recipes = useSelector((state) => state.recipes.items);
-  const totalItems = useSelector((state) => state.recipes.totalItems);
-
-  const [page, setPage] = useState(1);
-  const recipesPerPage = 12;
-  const [loading, setLoading] = useState(false);
-
-  const recipesListRef = useRef(null);
-
-  const [selectedFilters, setSelectedFilters] = useState({
-    category: "",
-    ingredient: "",
-  });
-
-  const handleFilterChange = useCallback((filters) => {
-    setSelectedFilters((prevFilters) => {
-      if (
-        prevFilters.category === filters.category &&
-        prevFilters.ingredient === filters.ingredient
-      ) {
-        return prevFilters;
-      }
-      return filters;
-    });
-    setPage(1);
-  }, []);
-
-  useEffect(() => {
-    setLoading(true);
-    dispatch(
-      fetchRecipes({
-        page,
-        perPage: recipesPerPage,
-        category: selectedFilters.category,
-        ingredient: selectedFilters.ingredient,
-      })
-    )
-      .unwrap()
-      .then(() => setLoading(false))
-      .catch(() => setLoading(false));
-  }, [dispatch, page, selectedFilters]);
-
-  const handleToggleFavorite = (id, add) => {
-    dispatch(toggleFavoriteRecipeAsync({ id, add }));
-  };
-
-  const loadMore = () => {
-    setPage((prev) => prev + 1);
-  };
-
-  useEffect(() => {
-    if (page > 1 && recipesListRef.current) {
-      setTimeout(() => {
-        requestAnimationFrame(() => {
-          const list = recipesListRef.current;
-          // Знаходимо останній елемент в списку
-          const lastRecipe = list.children[list.children.length - 1];
-          if (lastRecipe) {
-            lastRecipe.scrollIntoView({ behavior: "smooth", block: "start" });
-          }
-        });
-      }, 200);
+const RecipesList = forwardRef(
+  ({ recipes, loading, onToggleFavorite }, ref) => {
+    if (loading) {
+      return <Loading />;
     }
-  }, [page]);
 
-  const recipesToShow = recipes.slice(0, page * recipesPerPage);
+    if (!loading && recipes.length === 0) {
+      return <p className={styles.noRecipesText}>No recipes found.</p>;
+    }
 
-  return (
-    <section className={styles.RecipesList}>
-      <div className={styles.recipeListContainer}>
-        <div className={styles.FormRecipes}>
-          <h2 className={styles.Recipes}>Recipes</h2>
-        </div>
-
-        <Filters totalItems={totalItems} onChange={handleFilterChange} />
-
-        {loading ? (
-          <Loading />
-        ) : (
-          <>
-            {!loading && recipes.length === 0 && (
-              <p className={styles.noRecipesText}>No recipes found.</p>
-            )}
-
-            <div className={styles.recipeslist} ref={recipesListRef}>
-              {recipesToShow.map((recipe) => (
-                <RecipeCard
-                  key={recipe._id}
-                  recipe={recipe}
-                  onToggleFavorite={handleToggleFavorite}
-                />
-              ))}
-            </div>
-
-            <div className={styles.BtnLoadWrapper}>
-              <div className={styles.BtnLoad}>
-                {page * recipesPerPage < totalItems && (
-                  <LoadMoreBtn onClick={loadMore}>Load More</LoadMoreBtn>
-                )}
-              </div>
-            </div>
-          </>
-        )}
+    return (
+      <div className={styles.recipeslist} ref={ref}>
+        {recipes.map((recipe) => (
+          <RecipeCard
+            key={recipe._id}
+            recipe={recipe}
+            onToggleFavorite={onToggleFavorite}
+          />
+        ))}
       </div>
-    </section>
-  );
-};
+    );
+  }
+);
+RecipesList.displayName = "RecipesList";
 export default RecipesList;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,107 @@
-import css from "../HomePage/HomePage.module.css";
+// HomePage.jsx
+import { useState, useEffect, useRef, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import css from "./HomePage.module.css";
 import Hero from "../../components/Hero/Hero";
 import RecipesList from "../../components/RecipesList/RecipesList";
+import Filters from "../../components/Filters/Filters";
+import LoadMoreBtn from "../../components/LoadMoreBtn/LoadMoreBtn";
+import {
+  fetchRecipes,
+  toggleFavoriteRecipeAsync,
+} from "../../redux/recipes/operations";
 
 export default function HomePage() {
+  const dispatch = useDispatch();
+  const recipes = useSelector((state) => state.recipes.items);
+  const totalItems = useSelector((state) => state.recipes.totalItems);
+
+  const [page, setPage] = useState(1);
+  const recipesPerPage = 12;
+  const [loading, setLoading] = useState(false);
+  const [selectedFilters, setSelectedFilters] = useState({
+    category: "",
+    ingredient: "",
+  });
+
+  const recipesListRef = useRef(null);
+
+  const handleFilterChange = useCallback((filters) => {
+    setSelectedFilters((prevFilters) => {
+      if (
+        prevFilters.category === filters.category &&
+        prevFilters.ingredient === filters.ingredient
+      ) {
+        return prevFilters;
+      }
+      return filters;
+    });
+    setPage(1);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    dispatch(
+      fetchRecipes({
+        page,
+        perPage: recipesPerPage,
+        category: selectedFilters.category,
+        ingredient: selectedFilters.ingredient,
+      })
+    )
+      .unwrap()
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
+  }, [dispatch, page, selectedFilters]);
+
+  const handleToggleFavorite = (id, add) => {
+    dispatch(toggleFavoriteRecipeAsync({ id, add }));
+  };
+
+  const loadMore = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (page > 1 && recipesListRef.current) {
+      setTimeout(() => {
+        requestAnimationFrame(() => {
+          const list = recipesListRef.current;
+          const lastRecipe = list.children[list.children.length - 1];
+          if (lastRecipe) {
+            lastRecipe.scrollIntoView({ behavior: "smooth", block: "start" });
+          }
+        });
+      }, 200);
+    }
+  }, [page]);
+
+  const recipesToShow = recipes.slice(0, page * recipesPerPage);
+
   return (
     <div className={css.homePage}>
       <Hero />
-      <RecipesList />
+
+      <section>
+        <div>
+          <h2>Recipes</h2>
+        </div>
+
+        <Filters totalItems={totalItems} onChange={handleFilterChange} />
+
+        <RecipesList
+          recipes={recipesToShow}
+          loading={loading}
+          onToggleFavorite={handleToggleFavorite}
+          ref={recipesListRef}
+        />
+
+        <div>
+          {page * recipesPerPage < totalItems && !loading && (
+            <LoadMoreBtn onClick={loadMore}>Load More</LoadMoreBtn>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
