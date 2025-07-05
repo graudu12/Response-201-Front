d56ad7f0eb8b12ab38fdbde147aec9689b528eb0
Daryna <buriak.daryna@gmail.com>
2025-07-05 19:23:03 +0300

    add request to refresh instead of user/current to login and registration

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -53,7 +53,8 @@ export const logIn = createAsyncThunk(
       const accessToken = res.data.data.accessToken;
       setAuthHeader(accessToken);
       localStorage.setItem("token", accessToken);
-      const userRes = await axios.get("/user/current");
+      //const userRes = await axios.get("/user/current");
+      const userRes = await axios.get("/auth/refresh");
       return { accessToken, user: userRes.data || null };
     } catch (e) {
       return thunkAPI.rejectWithValue(
@@ -86,8 +87,8 @@ export const refreshUser = createAsyncThunk(
       }
       setAuthHeader(persistedToken);
 
-      //const res = await axios.get("/auth/refresh");
-      const res = await axios.get("/user/current");
+      const res = await axios.get("/auth/refresh");
+      //const res = await axios.get("/user/current");
       return res.data.data;
     } catch (error) {
       /*{
