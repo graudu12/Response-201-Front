c20852212a393cfe2c5fcea741d99c0131fc6b90
SPok <s.pokotilo@gmail.com>
2025-07-09 10:47:18 +0300

    modal

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,30 +1,12 @@
-// import { NavLink } from "react-router-dom";
-// import css from "./Logo.module.css";
-
-// const Logo = ({ className }) => {
-//   return (
-//     <NavLink to="/" className={`${css.logo} ${className || ""}`}>
-//       <svg className={css.logoIcon} width="30" height="30">
-//         <use href={`/svg/sprite.svg#icon-logo`} />
-//       </svg>
-//       <span className={css.logoText}>Tasteorama</span>
-//     </NavLink>
-//   );
-// };
-
-// export default Logo;
-
-
 import { NavLink } from "react-router-dom";
-import { useDispatch } from "react-redux"; 
-import { changeFilter } from "../../redux/filters/slice"; 
-import { clearNotFound } from "../../redux/recipes/slice"; 
+import { useDispatch } from "react-redux";
+import { changeFilter } from "../../redux/filters/slice";
+import { clearNotFound } from "../../redux/recipes/slice";
 import css from "./Logo.module.css";
 
 const Logo = ({ className }) => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
-  // + обработчик для сброса фильтра
   const handleClick = () => {
     dispatch(changeFilter({ name: "" }));
     dispatch(clearNotFound());
@@ -33,7 +15,7 @@ const Logo = ({ className }) => {
   return (
     <NavLink
       to="/"
-      onClick={handleClick} 
+      onClick={handleClick}
       className={`${css.logo} ${className || ""}`}
     >
       <svg className={css.logoIcon} width="30" height="30">
