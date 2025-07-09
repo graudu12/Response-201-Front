076359a3095e250f1246afa31aaa46f7cf05902a
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-07-09 14:17:39 +0300

    Fix SaveFavoriteButton and auth/recipes selectors

--- src/components/SaveFavoriteButton/SaveFavoriteButton.jsx
+++ src/components/SaveFavoriteButton/SaveFavoriteButton.jsx
@@ -1,52 +1,127 @@
-import { useState, useEffect } from "react";
+// import { useState, } from "react";
+// import styles from "./SaveFavoriteButton.module.css";
+// import { toast } from "react-toastify";
+// import { useDispatch, useSelector } from "react-redux";
+// import { toggleFavoriteRecipeAsync } from "../../redux/recipes/operations";
+// import { selectIsLoggedIn } from "../../redux/auth/selectors";
+// import AuthPromptModal from "../AuthPromptModal/AuthPromptModal";
+
+// function SaveFavoriteButton({ small, recipeId, mode }) {
+//   const dispatch = useDispatch();
+//   const isLoggedIn = useSelector(selectIsLoggedIn);
+//   const favoriteRecipes = useSelector(
+//     (state) => state.auth.user?.favoriteRecipes ?? []
+//   );
+
+//   const [hovered, setHovered] = useState(false);
+//   const [showModal, setShowModal] = useState(false);
+
+//   const isFavorite = favoriteRecipes.includes(recipeId); // ❗ вычисляем напрямую
+
+//   const handleToggle = async () => {
+//     if (!isLoggedIn) {
+//       setShowModal(true);
+//       return;
+//     }
+
+//     try {
+//       await dispatch(toggleFavoriteRecipeAsync({ recipeId, mode })).unwrap();
+//     } catch (error) {
+//       if (!isFavorite) {
+//         toast.error("Failed to add to favorites 😢");
+//       } else {
+//         toast.error("Failed to remove from favorites 😢");
+//       }
+//     }
+//   };
+
+//   const handleMouseEnter = () => {
+//     if (small && isFavorite) setHovered(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setHovered(false);
+//   };
+
+//   const iconId = hovered ? "icon-delete" : "icon-saved";
+
+//   return (
+//     <>
+//       <button
+//         onClick={handleToggle}
+//         className={`${
+//           small ? styles.smallFavoriteButton : styles.favoriteButton
+//         } ${isFavorite ? styles.active : ""}`}
+//         aria-label={isFavorite ? "Remove" : "Add to favorites"}
+//         onMouseEnter={small ? handleMouseEnter : undefined}
+//         onMouseLeave={small ? handleMouseLeave : undefined}
+//       >
+//         <svg
+//           className={`${styles.icon} ${
+//             !small && isFavorite ? styles.iconFilledWhite : ""
+//           }`}
+//           width="24"
+//           height="24"
+//         >
+//           <use href={`/svg/sprite.svg#${iconId}`} />
+//         </svg>
+//         {!small && (isFavorite ? "Remove" : "Save")}
+//       </button>
+
+//       <AuthPromptModal isOpen={showModal} onClose={() => setShowModal(false)} />
+//     </>
+//   );
+// }
+
+// export default SaveFavoriteButton;
+
+// src/components/SaveFavoriteButton/SaveFavoriteButton.jsx
+import { useState } from "react";
+import { createSelector } from "@reduxjs/toolkit";
+
 import styles from "./SaveFavoriteButton.module.css";
 import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleFavoriteRecipeAsync } from "../../redux/recipes/operations";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import {
+  selectIsLoggedIn,
+  selectFavoriteRecipes,
+} from "../../redux/auth/selectors";
 import AuthPromptModal from "../AuthPromptModal/AuthPromptModal";
-import Loading from "../Loading/Loading";
-function SaveFavoriteButton({ small, recipeId, mode }) {
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const favoriteRecipes = useSelector(
-    (state) => state.auth.user?.favoriteRecipes ?? []
-  );
 
-  //const [hovered, setHovered] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [isFavorite, setIsFavorite] = useState(
+const makeSelectIsFavorite = (recipeId) =>
+  createSelector([selectFavoriteRecipes], (favoriteRecipes) =>
     favoriteRecipes.includes(recipeId)
   );
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setIsFavorite(favoriteRecipes.includes(recipeId));
-  }, [favoriteRecipes, recipeId]);
+function SaveFavoriteButton({ small, recipeId, mode }) {
+  const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const isFavorite = useSelector(makeSelectIsFavorite(recipeId));
+
+  const [hovered, setHovered] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
   const handleToggle = async () => {
-    const wasFavorite = isFavorite;
     if (!isLoggedIn) {
       setShowModal(true);
       return;
     }
-    const nextFavorite = !isFavorite;
-    setIsFavorite(nextFavorite); // миттєва зміна
-    setIsLoading(true);
+
     try {
       await dispatch(toggleFavoriteRecipeAsync({ recipeId, mode })).unwrap();
     } catch (error) {
-      setIsFavorite(wasFavorite);
       if (!isFavorite) {
         toast.error("Failed to add to favorites 😢");
-      } else if (wasFavorite) {
+      } else {
         toast.error("Failed to remove from favorites 😢");
       }
-    } finally {
-      setIsLoading(false);
     }
   };
 
-  /* const handleMouseEnter = () => {
+  const handleMouseEnter = () => {
     if (small && isFavorite) setHovered(true);
   };
 
@@ -54,7 +129,7 @@ function SaveFavoriteButton({ small, recipeId, mode }) {
     setHovered(false);
   };
 
-  const iconId = hovered ? "icon-delete" : "icon-saved";*/
+  const iconId = hovered ? "icon-delete" : "icon-saved";
 
   return (
     <>
@@ -64,31 +139,22 @@ function SaveFavoriteButton({ small, recipeId, mode }) {
           small ? styles.smallFavoriteButton : styles.favoriteButton
         } ${isFavorite ? styles.active : ""}`}
         aria-label={isFavorite ? "Remove" : "Add to favorites"}
-        //onMouseEnter={small ? handleMouseEnter : undefined}
-        //onMouseLeave={small ? handleMouseLeave : undefined}
-        disabled={isLoading} // 👈 Блокуємо кнопку під час запиту
+        onMouseEnter={small ? handleMouseEnter : undefined}
+        onMouseLeave={small ? handleMouseLeave : undefined}
       >
-        {isLoading ? (
-          <div>
-            <Loading height={60} width={60} />
-          </div>
-        ) : (
-          <>
-            <svg
-              className={`${styles.icon} ${
-                !small && isFavorite ? styles.iconFilledWhite : ""
-              }`}
-              width="24"
-              height="24"
-            >
-              <use href="/svg/sprite.svg#icon-saved" />
-              {/*<use href={`/svg/sprite.svg#${iconId}`} />*/}
-            </svg>
-            {!small && (isFavorite ? "Remove" : "Save")}
-          </>
-        )}
+        <svg
+          className={`${styles.icon} ${
+            !small && isFavorite ? styles.iconFilledWhite : ""
+          }`}
+          width="24"
+          height="24"
+        >
+          <use href={`/svg/sprite.svg#${iconId}`} />
+        </svg>
+        {!small && (isFavorite ? "Remove" : "Save")}
       </button>
 
+      {/* Модальное окно для аутентификации */}
       <AuthPromptModal isOpen={showModal} onClose={() => setShowModal(false)} />
     </>
   );
